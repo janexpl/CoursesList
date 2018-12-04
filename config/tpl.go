201f70a3ff5b795337fe8b35d4de588190a1b00e
janusz <janexpl@me.com>
2018-12-04 10:22:12 +0100

    added funcionality

--- config/tpl.go
+++ config/tpl.go
@@ -1,9 +1,113 @@
 package config
 
-import "html/template"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
 
-var TPL *template.Template
+type Layout string
+
+var layout *template.Template
+
+const (
+	trimPrefix        = "templates/"
+	Home       Layout = "home"
+	Ajax       Layout = "ajax"
+	User       Layout = "user"
+)
+
+var (
+	layoutFuncs = template.FuncMap{
+		"yield": func() (string, error) {
+			return "", fmt.Errorf("yield called unexpectedly")
+		},
+		"dateformat":  dateformat,
+		"formdatefmt": formdatefmt,
+	}
+)
+
+func dateformat(t time.Time) string {
+	return t.Format("02.01.2006")
+}
+func formdatefmt(t time.Time) string {
+
+	return t.Format("2006-01-02")
+}
+
+var errorTemplate = `
+<html>
+	<body>
+		<h1>Error rendering template %s</h1>
+		<p>%s</p>
+	</body>
+</html>
+`
+var templates *template.Template
 
 func init() {
-	TPL = template.Must(template.ParseGlob("templates/*.gohtml"))
+	layout = template.Must(
+		template.
+			New("layout.gohtml").
+			Funcs(layoutFuncs).
+			ParseFiles("templates/layout.gohtml", "templates/header.gohtml"))
+	for _, t := range layout.Templates() {
+		fmt.Println(t.Name())
+	}
+	templates = template.Must(
+		template.
+			New("t").Funcs(layoutFuncs).
+			ParseGlob("templates/*/*.gohtml"))
+	//	lms, err := filepath.Glob("templates/*.gohtml")
+	/* if err != nil {
+		log.Fatal(err)
+	}
+	for _, lm := range lms {
+		b, err := ioutil.ReadFile(lm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		name := strings.TrimPrefix(lm, trimPrefix)
+		template.Must(LYT.New(name).Parse(string(b)))
+	}*/
+
+}
+
+func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data map[string]interface{}) {
+	if data == nil {
+		data = map[string]interface{}{}
+	}
+
+	//data["CurrentUser"] = RequestUser(r)
+	//data["Flash"] = r.URL.Query().Get("flash")
+	templates = template.Must(
+		template.
+			New("t").Funcs(layoutFuncs).
+			ParseGlob("templates/*/*.gohtml"))
+
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error) {
+			buf := bytes.NewBuffer(nil)
+			err := templates.ExecuteTemplate(buf, name, data)
+			return template.HTML(buf.String()), err
+		},
+		"dateformat": dateformat,
+	}
+
+	layoutClone, _ := layout.Clone()
+
+	layoutClone.Funcs(funcs)
+
+	err := layoutClone.Execute(w, data)
+
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf(errorTemplate, name, err),
+			http.StatusInternalServerError,
+		)
+	}
+
 }
--- main.go
+++ main.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/janexpl/CoursesList/config"
+	"github.com/janexpl/CoursesList/controllers"
+	"github.com/janexpl/CoursesList/logging"
+)
+
 func main() {
+
+	courses := controllers.NewCoursesController()
+
+	http.HandleFunc("/", index)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.HandleFunc("/courses/create/process", courses.CreateProcess)
+	http.HandleFunc("/courses/create", courses.Create)
+	http.HandleFunc("/courses/show", courses.Show)
+	http.HandleFunc("/courses/delete/process", courses.DeleteProcess)
+	http.HandleFunc("/courses", courses.Index)
+	http.HandleFunc("/courses/update/process", courses.UpdateProcess)
+	http.HandleFunc("/courses/update", courses.Update)
+	http.HandleFunc("/courses/getcoursejson", courses.GetCourseJson)
+	students := controllers.NewStudentsController()
+	http.HandleFunc("/students/create/process", students.CreateProcess)
+	http.HandleFunc("/students/create", students.Create)
+	http.HandleFunc("/students/show", students.Show)
+	http.HandleFunc("/students/delete/process", students.DeleteProcess)
+	http.HandleFunc("/students", students.Index)
+	http.HandleFunc("/students/getalljson", students.GetAllJson)
+	http.HandleFunc("/students/update/process", students.UpdateProcess)
+	http.HandleFunc("/students/update", students.Update)
+	companies := controllers.NewCompaniesController()
+	http.HandleFunc("/companies/create/process", companies.CreateProcess)
+	http.HandleFunc("/companies/create", companies.Create)
+	http.HandleFunc("/companies/show", companies.Show)
+	http.HandleFunc("/companies/delete/process", companies.DeleteProcess)
+	http.HandleFunc("/companies", companies.Index)
+	http.HandleFunc("/companies/update/process", companies.UpdateProcess)
+	http.HandleFunc("/companies/update", companies.Update)
+	http.HandleFunc("/companies/getalljson", companies.GetAllJson)
+	http.HandleFunc("/companies/addfrommodal", companies.CreateFromModal)
+	certificates := controllers.NewCertificatesController()
+	http.HandleFunc("/certificates", certificates.Index)
+	http.HandleFunc("/certificates/create", certificates.Create)
+	http.HandleFunc("/certificates/create/process", certificates.CreateProcess)
+	http.HandleFunc("/certificates/delete/process", certificates.DeleteProcess)
+
+	/*
+
+		http.HandleFunc("/certificates/show", certificates.Show)
+
+
+		http.HandleFunc("/certificates/update/process", certificates.UpdateProcess)
+		http.HandleFunc("/certificates/update", certificates.Update)
+		http.HandleFunc("/certificates/getalljson", certificates.GetAllJson)*/
+	registries := controllers.NewRegistriesController()
+	//http.HandleFunc("/certificates", certificates.Index)
+	http.HandleFunc("/registries/getlastnumber", registries.GetLastNumberWithSymbol)
+	logging.Info.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	config.RenderTemplate(w, r, "index/home", nil)
 
 }
